5513f2a71f6c3d8318946cd05b3dd6366c321f2d
dogmania <hj296914@gmail.com>
2024-10-21 17:53:45 +0900

    :bento: Chore: 디자인 리소스 추가

---
+++ Poptato_iOS/Poptato_iOS/Scene/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Poptato_iOS
+//
+//  Created by 현수 노트북 on 10/21/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
