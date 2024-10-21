5e41ff2333a6bbb94be41e5a242c5dd8373f2480
dogmania <hj296914@gmail.com>
2024-10-21 16:37:34 +0900

    :sparkles: Feat: 프로젝트 생성

---
+++ Poptato_iOS/Poptato_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Poptato_iOS
+//
+//  Created by 현수 노트북 on 10/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Poptato_iOS/Poptato_iOSUITests/Poptato_iOSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Poptato_iOSUITestsLaunchTests.swift
+//  Poptato_iOSUITests
+//
+//  Created by 현수 노트북 on 10/21/24.
+//
+
+import XCTest
+
+final class Poptato_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
