b6bf1f9079416b336f0afa8e85cc58a14c8bbe4f
dogmania <hj296914@gmail.com>
2024-10-27 00:40:02 +0900

    :bento: Chore: 리소스 추가

---
+++ Poptato_iOS/Poptato_iOS/Scene/Backlog/BottomSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  BottomSheetView.swift
+//  Poptato_iOS
+//
+//  Created by 현수 노트북 on 10/26/24.
+//
+
+import SwiftUI
+
+struct BottomSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomSheetView()
+}
