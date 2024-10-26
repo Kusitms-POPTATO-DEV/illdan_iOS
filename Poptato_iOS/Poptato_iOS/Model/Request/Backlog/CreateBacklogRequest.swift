b2f17ddc7a03b5dfe57ab36477966a6d464f288c
dogmania <hj296914@gmail.com>
2024-10-26 20:21:55 +0900

    :sparkles: Feat: 백로그 요청,응답 구조체 구현

---
+++ Poptato_iOS/Poptato_iOS/Model/Request/Backlog/CreateBacklogRequest.swift
@@ -0,0 +1,10 @@
+//
+//  CreateBacklogRequest.swift
+//  Poptato_iOS
+//
+//  Created by 현수 노트북 on 10/26/24.
+//
+
+struct CreateBacklogRequest: Codable {
+    let content: String
+}
---
+++ Poptato_iOS/Poptato_iOS/Model/Response/Todo/TodoIdModel.swift
@@ -0,0 +1,10 @@
+//
+//  TodoIdModel.swift
+//  Poptato_iOS
+//
+//  Created by 현수 노트북 on 10/26/24.
+//
+
+struct TodoIdModel: Codable {
+    let todoId: Int
+}
