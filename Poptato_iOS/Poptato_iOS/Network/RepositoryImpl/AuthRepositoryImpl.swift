d0844f2e5ad9db0a0bb6f3304ec5b379034bfc32
dogmania <hj296914@gmail.com>
2024-10-26 19:47:49 +0900

    :sparkles: Feat: 토큰 갱신 api 구현

--- Poptato_iOS/Poptato_iOS/Network/RepositoryImpl/AuthRepositoryImpl.swift
+++ Poptato_iOS/Poptato_iOS/Network/RepositoryImpl/AuthRepositoryImpl.swift
@@ -9,4 +9,8 @@ final class AuthRepositoryImpl: AuthRepository {
     func kakaoLogin(request: KaKaoLoginRequest) async throws -> KaKaoLoginResponse {
         try await NetworkManager.shared.request(type: KaKaoLoginResponse.self, api: .kakaoLogin(loginRequest: request))
     }
+    
+    func refreshToken(request: TokenModel) async throws -> TokenModel {
+        try await NetworkManager.shared.request(type: TokenModel.self, api: .reissueToken(reissueRequest: request))
+    }
 }
