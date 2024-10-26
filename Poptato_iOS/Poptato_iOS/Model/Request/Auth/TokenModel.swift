9110c847d379f013390008e053b343b23ee85243
dogmania <hj296914@gmail.com>
2024-10-26 19:47:19 +0900

    :sparkles: Feat: 토큰 요청, 응답 구조체 구현

---
+++ Poptato_iOS/Poptato_iOS/Model/Request/Auth/TokenModel.swift
@@ -0,0 +1,11 @@
+//
+//  ReissueTokenRequest.swift
+//  Poptato_iOS
+//
+//  Created by 현수 노트북 on 10/26/24.
+//
+
+struct TokenModel: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
