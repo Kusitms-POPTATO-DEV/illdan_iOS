1122b7cef459da97ff96cd0a7e5e0dd7f9ba6f2a
dogmania <hj296914@gmail.com>
2024-11-28 15:27:58 +0900

    :sparkles: Feat: SVG 이미지 렌더링 뷰 구현

--- Poptato_iOS/Poptato_iOS/Util/AsyncImageView.swift
+++ Poptato_iOS/Poptato_iOS/Util/AsyncImageView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SVGKit
 
 struct AsyncImageView: View {
     let imageURL: String
@@ -31,5 +32,66 @@ struct AsyncImageView: View {
             .frame(width: width, height: height)
             .clipShape(Circle())
         }
+    }
+}
+
+struct SVGImageView: View {
+    let imageURL: String
+    var width: CGFloat
+    var height: CGFloat
+    @State private var svgImage: SVGKImage? = nil
+    @State private var isLoading = true
+
+    var body: some View {
+        Group {
+            if let svgImage = svgImage {
+                Image(uiImage: svgImage.uiImage)
+                    .resizable()
+                    .frame(width: width, height: height)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } else if isLoading {
+                ProgressView()
+                    .frame(width: width, height: height)
+            } else {
+                Text("SVG 로드 실패")
+                    .frame(width: width, height: height)
+            }
+        }
+        .onAppear {
+            loadSVGImage(from: imageURL)
+        }
+        .onChange(of: imageURL) {
+            isLoading = true
+            svgImage = nil
+            loadSVGImage(from: imageURL)
+        }
+    }
+
+    private func loadSVGImage(from urlString: String) {
+        guard let url = URL(string: urlString) else {
+            isLoading = false
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async {
+                    isLoading = false
+                }
+                return
+            }
+            
+            if let svgImage = SVGKImage(data: data) {
+                DispatchQueue.main.async {
+                    self.svgImage = svgImage
+                    self.isLoading = false
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.isLoading = false
+                }
+            }
+        }.resume()
     }
 }
