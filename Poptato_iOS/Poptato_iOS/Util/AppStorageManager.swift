52a3150157ed373adeaaed5b85e914f3045942c8
dogmania <hj296914@gmail.com>
2025-02-01 22:43:22 +0900

    :sparkles: Feat: AppStorage 관련 로직을 관리하고 어제 한 일 페이지의 플래그 변수를 관리하는 로직 구현

---
+++ Poptato_iOS/Poptato_iOS/Util/AppStorageManager.swift
@@ -0,0 +1,34 @@
+//
+//  AppStorageManager.swift
+//  Poptato_iOS
+//
+//  Created by 현수 노트북 on 2/1/25.
+//
+
+import SwiftUI
+
+struct AppStorageManager {
+    private static let hasSeenYesterdayKey = "hasSeenYesterdayView"
+    private static let lastUpdatedDateKey = "lastUpdatedDate"
+    
+    static var hasSeenYesterday: Bool {
+        get {
+            checkAndResetIfNewDay()
+            return UserDefaults.standard.bool(forKey: hasSeenYesterdayKey)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: hasSeenYesterdayKey)
+            UserDefaults.standard.set(TimeFormatter.currentDateString(), forKey: lastUpdatedDateKey) 
+        }
+    }
+    
+    private static func checkAndResetIfNewDay() {
+        let lastUpdatedDate = UserDefaults.standard.string(forKey: lastUpdatedDateKey) ?? ""
+        let today = TimeFormatter.currentDateString()
+
+        if lastUpdatedDate != today {
+            UserDefaults.standard.set(false, forKey: hasSeenYesterdayKey)
+            UserDefaults.standard.set(today, forKey: lastUpdatedDateKey)
+        }
+    }
+}
