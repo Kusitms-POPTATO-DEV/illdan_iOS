a13a3ff1d80b5a2cf2b25c9f0a485e538479105d
dogmania <hj296914@gmail.com>
2025-04-06 22:17:07 +0900

    :recycle: Refactor: 스플래시 로고 변경

--- Poptato_iOS/Poptato_iOS/Scene/Splash/SplashView.swift
+++ Poptato_iOS/Poptato_iOS/Scene/Splash/SplashView.swift
@@ -12,24 +12,8 @@ struct SplashView: View {
         ZStack{
             Color.gray100
                 .edgesIgnoringSafeArea(.all)
-            Color.splash
-                .edgesIgnoringSafeArea(.all)
             
-            VStack {
-                Image("ic_splash")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 137)
-                    .offset(y: 80)
-                
-                Spacer()
-                
-                Image("ic_stairs")
-                    .resizable()
-                    .ignoresSafeArea()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: .infinity)
-            }
+            Image("ic_splash")
         }
     }
 }
