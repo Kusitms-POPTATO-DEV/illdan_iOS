caeb51f3b318ff7df15372e2d47d5b151d0b230d
dogmania <hj296914@gmail.com>
2025-04-16 17:17:13 +0900

    :recycle: Refactor: 바텀시트 달력 디자인 수정

--- Poptato_iOS/Poptato_iOS/Scene/Main/BottomSheetView.swift
+++ Poptato_iOS/Poptato_iOS/Scene/Main/BottomSheetView.swift
@@ -114,46 +114,9 @@ struct BottomSheetView: View {
                             }
                         )
                     )
-//                    BottomSheetButton(
-//                        image: "ic_pen",
-//                        buttonText: "수정하기",
-//                        buttonColor: .gray30,
-//                        subText: "",
-//                        onClickBtn: {
-//                            AnalyticsManager.shared.logEvent(AnalyticsEvent.edit_task)
-//                            isVisible = false
-//                            editTodo()
-//                        },
-//                        isRepeat: Binding(
-//                            get: { todoItem?.isRepeat ?? false },
-//                            set: { newValue in
-//                                todoItem?.isRepeat = newValue
-//                            }
-//                        )
-//                    )
-//                    BottomSheetButton(
-//                        image: "ic_trash",
-//                        buttonText: "삭제하기",
-//                        buttonColor: .danger50,
-//                        subText: "",
-//                        onClickBtn: {
-//                            AnalyticsManager.shared.logEvent(AnalyticsEvent.delete_task, parameters: ["task_id" : todoItem?.todoId ?? -1])
-//                            isVisible = false
-//                            deleteTodo()
-//                        },
-//                        isRepeat: Binding(
-//                            get: { todoItem?.isRepeat ?? false },
-//                            set: { newValue in
-//                                todoItem?.isRepeat = newValue
-//                            }
-//                        )
-//                    )
-                    
-//                    Spacer()
                 }
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
-//                .frame(height: 350)
                 .background(Color(UIColor.gray100))
                 .clipShape(RoundedCorner(radius: 24))
             }
@@ -247,7 +210,8 @@ struct DateBottomSheet: View {
             Spacer()
             VStack(spacing: 0) {
                 Spacer().frame(height: 24)
-                DateNavigatorView(
+                
+                BottomSheetDateNavigatorView(
                     year: selectedYear,
                     month: selectedMonth,
                     onClickIncreaseMonth: {
@@ -263,6 +227,7 @@ struct DateBottomSheet: View {
                         generateCalendarDays()
                     }
                 )
+                
                 Spacer().frame(height: 16)
                 
                 BottomSheetCalendarView(
@@ -289,9 +254,10 @@ struct DateBottomSheet: View {
                 )
             }
             .frame(maxWidth: .infinity)
+            .padding(.horizontal, 30)
             .fixedSize(horizontal: false, vertical: true)
             .background(Color(UIColor.gray100))
-            .clipShape(RoundedCorner(radius: 16, corners: [.topLeft, .topRight]))
+            .clipShape(RoundedCorner(radius: 24))
         }
         .onAppear {
             initializeSelectedDate()
@@ -345,7 +311,6 @@ struct BottomSheetCalendarView: View {
                         .foregroundColor(.gray60)
                 }
             }
-            .padding(.horizontal, 24)
             
             LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7), spacing: 10) {
                 ForEach(Array(days.enumerated()), id: \.offset) { index, date in
@@ -353,10 +318,10 @@ struct BottomSheetCalendarView: View {
                         Text("\(date)")
                             .frame(width: 32, height: 32)
                             .font(PoptatoTypo.smMedium)
-                            .foregroundColor(selectedDay == date ? .gray100 : .gray70)
+                            .foregroundColor(selectedDay == date ? .gray100 : .gray10)
                             .background(
                                 Rectangle()
-                                    .fill(selectedDay == date ? Color.primary60 : Color.gray100)
+                                    .fill(selectedDay == date ? Color.primary40 : Color.gray100)
                                     .cornerRadius(8)
                             )
                             .onTapGesture {
@@ -369,7 +334,6 @@ struct BottomSheetCalendarView: View {
                     }
                 }
             }
-            .padding(.horizontal, 24)
             
             Spacer().frame(height: 24)
             
@@ -388,6 +352,46 @@ struct BottomSheetCalendarView: View {
             
             Spacer().frame(height: 16)
         }
+    }
+}
+
+struct BottomSheetDateNavigatorView: View {
+    let year: Int
+    let month: Int
+    
+    var onClickIncreaseMonth: () -> Void
+    var onClickDecreaseMonth: () -> Void
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            
+            Image("ic_arrow_left")
+                .renderingMode(.template)
+                .resizable()
+                .frame(width: 20, height: 20)
+                .foregroundColor(.gray40)
+                .onTapGesture {
+                    onClickDecreaseMonth()
+                }
+            
+            Spacer()
+            
+            Text("\(String(format: "%d", year))년 \(String(format: "%2d", month))월")
+                .font(PoptatoTypo.mdMedium)
+                .foregroundColor(.gray00)
+            
+            Spacer()
+            
+            Image("ic_arrow_right")
+                .renderingMode(.template)
+                .resizable()
+                .frame(width: 20, height: 20)
+                .foregroundColor(.gray40)
+                .onTapGesture {
+                    onClickIncreaseMonth()
+                }
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
@@ -511,7 +515,6 @@ struct BottomSheetActionButton: View {
             }
         }
         .frame(maxWidth: .infinity)
-        .padding(.horizontal, 24)
         .padding(.vertical, 8)
     }
 }
