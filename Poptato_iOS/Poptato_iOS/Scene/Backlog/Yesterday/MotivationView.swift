1edd0480a6c2e8586deb16784ea996a84fc748d2
dogmania <hj296914@gmail.com>
2025-04-18 11:07:14 +0900

    :recycle: Refactor: 동기부여 화면 디자인 수정

--- Poptato_iOS/Poptato_iOS/Scene/Backlog/Yesterday/MotivationView.swift
+++ Poptato_iOS/Poptato_iOS/Scene/Backlog/Yesterday/MotivationView.swift
@@ -11,14 +11,16 @@ struct MotivationView: View {
     var body: some View {
         ZStack(alignment: .center) {
             Color.gray100.ignoresSafeArea()
-            Image("bg_motivation_view")
-                .resizable()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
             
-            Text("좋아요! 오늘 하루도\n힘차게 시작해볼까요?")
-                .multilineTextAlignment(.center)
-                .font(PoptatoTypo.xxLMedium)
-                .foregroundColor(.gray10)
+            VStack(spacing: 0) {
+                Text("좋아요! 오늘 하루도\n힘차게 시작해볼까요?")
+                    .multilineTextAlignment(.center)
+                    .font(PoptatoTypo.xLSemiBold)
+                    .foregroundColor(.gray00)
+                
+                Image("ic_motivation_fire")
+            }
+            .frame(maxWidth: .infinity)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
