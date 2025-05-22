8adabd7e65d1dc96c2d6f29cd236833d7fe04f62
dogmania <hj296914@gmail.com>
2025-05-23 04:21:49 +0900

    :sparkles: Feat: 시간 설정 요청 구조체 구현

---
+++ Poptato_iOS/Poptato_iOS/Model/Request/Todo/Time/TimeInfo.swift
@@ -0,0 +1,16 @@
+//
+//  TimeInfo.swift
+//  Poptato_iOS
+//
+//  Created by 현수 노트북 on 5/23/25.
+//
+
+struct TimeInfo {
+    let meridiem: String
+    let hour: Int
+    let minute: Int
+}
+
+struct TodoTimeRequest: Codable {
+    let time: String
+}
