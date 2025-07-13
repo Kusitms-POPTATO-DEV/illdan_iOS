e16b39e0a3c1d9d56284dcc2d70620032ce4e9b6
dogmania <hj296914@gmail.com>
2025-07-14 01:17:55 +0900

    :sparkles: Feat: 파일 생성

---
+++ Poptato_iOS/Poptato_iOS/Scene/MyPage/comment/UserCommentView.swift
@@ -0,0 +1,18 @@
+//
+//  UserCommentView.swift
+//  Poptato_iOS
+//
+//  Created by 현수 노트북 on 7/13/25.
+//
+
+import SwiftUI
+
+struct UserCommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserCommentView()
+}
